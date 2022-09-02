bcc1a2fbe06c07a02b053fd2278ebe82857521f4
jonah lindsley <jonahlindsley@yahoo.com>
2022-09-02 10:07:09 -0500

    anded to the server ans schemas

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,49 @@
-const { User } = require('./models')
+const { User } = require('./')
+const { signToken } = require('../auth');
+const { ApolloError } = require('apollo-server-express');
+const { default: App } = require('../../../../../notes/react/react_playground_uofm/client/src/App');
 
 const resolvers = {
     Mutation: {
         async addUser(_, { email, password }, context) {
-            return await User.create({
-                email,
-                password,
-            });
+            try {
+                const user = await User.create({ email, password });
+
+                const token = await signToken(user);
+                return {user, token};
+            } catch (err) {
+                throw new ApolloError(err);
+            }
         },
+
+        async loginUser(_, {email, password }, context) {
+            try {
+                const user = await User.create({email, password});
+
+                const token = signToken(user);
+                return { user, token };
+            } catch (err){
+                throw new ApolloError(err);
+            }
+        },
+
+        async loginUser(_, { email, password }, context) {
+            const user = await User.findOne({ email });
+
+            if (!user) throw new ApolloError('no user found with that email')
+
+            if (!user.validatePass(password)) throw new ApolloError('your password is incorrect');
+
+            try {
+                const token = signToken(user);
+
+                return { user, token };
+            } catch (err) {
+                throw new ApolloError(err);
+            }
+        },
+
     }
-}
+};
+
+module.exports = resolvers;
