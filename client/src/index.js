e63cb6f2a65e3fcbbb63f2f06d2bced5c77f4aaf
odinarussell@gmail.com <odinarussel@gmail.com>
2022-09-06 17:22:28 -0500

    inported additional packages

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import './index.css';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
+  // link: ApolloLink.from([errorLink, authLink.concat(httpLink)]),
   cache: new InMemoryCache()
 });
 
@@ -21,4 +22,7 @@ root.render(
   </React.StrictMode>
 );
 
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
